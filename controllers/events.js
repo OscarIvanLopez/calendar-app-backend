9f4ce98955ebed4075eba6310b1ce006f08eef54
Oscar Ivan <oscari.lopez@potros.itson.edu.mx>
2021-04-18 12:09:37 -0700

    18/04/2021 bug fixed

--- controllers/events.js
+++ controllers/events.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 const Event = require("../models/Event");
 
 const getEvents = async (req, res = response) => {
-  const events = await Evento.find().populate("user", "name");
+  const events = await Event.find().populate("user", "name");
 
   res.json({
     ok: true,
